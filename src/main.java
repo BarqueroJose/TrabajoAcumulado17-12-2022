3d2fc1808ee00d1248610bbf4f4070e2383b7e2f
BarqueroJose <josebarquerolazo12@gmail.com>
2022-12-17 17:51:22 -0600

    Cargando Trabajo

---
+++ src/main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author barquero
+ */
+public class main {
+    public static void main(String[] args) {
+     frmlogin2 frm = new frmlogin2();
+       frm.setVisible(true);
+       frm.setLocationRelativeTo(null);
+        
+        
+    }
+
+    
+}
